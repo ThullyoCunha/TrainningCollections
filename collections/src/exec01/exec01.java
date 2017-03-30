56e8eba435f138bd7cd96225c921ea90053dbb24
panda <panda@panda-usto>
2017-03-30 00:04:45 -0300

    Começo do estudo de Collections.

---
+++ collections/src/exec01/exec01.java
@@ -0,0 +1,32 @@
+package exec01;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class exec01 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Criando uma lista e determinando que o tipo do objeto que ela vai conter será String;
+		List<String> list = new ArrayList<>();
+		 
+		list.add("Alan");
+		list.add("Breno");
+		list.add("Thullyo");
+		
+		//Listando quantos objetos a arraylist possui;
+		System.out.println(list.size());
+		
+		//Listando os objetos da arraylist;
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+	    }
+		
+		//Listando os objetos da arraylist toString;
+		System.out.println(list);
+		
+
+	}
+
+}
